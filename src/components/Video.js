f360b4b840a9aebca2cb8a79b7a6f2ccb5a8ae61
HugoDeSouzaCaramez <hugo.7.musica@gmail.com>
2023-11-26 18:41:23 -0300

    Tornando os Componentes Dinâmicos

--- src/components/Video.js
+++ src/components/Video.js
@@ -2,7 +2,15 @@ import React, { useRef, useState } from 'react';
 import './Video.css';
 import VideoFooter from './VideoFooter';
 import VideoSidebar from './VideoSidebar';
-const Video = () => {
+const Video = ({
+  url,
+  channel,
+  description,
+  song,
+  likes,
+  shares,
+  messages,
+}) => {
   const [playing, setPlaying] = useState(false);
   const videoRef = useRef(null);
   const handleVideoPress = () => {
@@ -17,14 +25,14 @@ const Video = () => {
   return (
     <div className="video">
       <video
-        src="https://res.cloudinary.com/dxkxvfo2o/video/upload/v1608169738/video1_cvrjfm.mp4"
+        src={url}
         className="video__player"
         loop
         ref={videoRef}
         onClick={handleVideoPress}
       ></video>
-      <VideoFooter />
-      <VideoSidebar />
+      <VideoFooter channel={channel} description={description} song={song} />
+      <VideoSidebar likes={likes} shares={shares} messages={messages} />
     </div>
   );
 };
--- src/components/VideoFooter.js
+++ src/components/VideoFooter.js
@@ -2,19 +2,19 @@ import React from 'react';
 import './VideoFooter.css';
 import MusicNoteIcon from '@material-ui/icons/MusicNote';
 import Ticker from 'react-ticker';
-const VideoFooter = () => {
+const VideoFooter = ({ channel, description, song }) => {
   return (
     <div className="videoFooter">
       <div className="videoFooter__text">
-        <h3>@nabendu82</h3>
-        <p>Macbook Air to new Windows editing beast</p>
+        <h3>@{channel}</h3>
+        <p>{description}</p>
         <div className="videoFooter__ticker">
           <MusicNoteIcon className="videoFooter__icon" />
           {/*Causava bug na renderização com react-dom >= 18 por isso npm install react@17.0.2 react-dom@17.0.2*/}
           <Ticker mode="smooth">
             {({ index }) => (
               <>
-                <p>I am a Windows PC</p>
+                <p>{song}</p>
               </>
             )}
           </Ticker>
